4f9658e68c2d36afb70450859ad3d7b1e35966a0
dantilse <dantilse@gmail.com>
2018-03-02 15:19:31 -0600

    Video 121 - create test for ExpenseForm component

--- src/tests/components/ExpenseListItem.test.js
+++ src/tests/components/ExpenseListItem.test.js
@@ -6,4 +6,4 @@ import expenses from '../fixtures/expenses';
 test('should render ExpenseListItem component', () => {
   const wrapper = shallow(<ExpenseListItem {...expenses[0]}/>);
   expect(wrapper).toMatchSnapshot();
-})
+});
