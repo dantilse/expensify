4e696a90ca12e09b91cec80bef0373273163d777
dantilse <dantilse@gmail.com>
2018-03-29 22:11:09 -0500

    Update moment format and add Numeral for currency dispay
    Video 138: Add Numeral to display currencies and update Expense List
    Item component and test.

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import moment from 'moment';
+import numeral from 'numeral';
 
 const ExpenseListItem = ({id, description, amount, createdAt}) => (
   <li>
@@ -7,7 +9,11 @@ const ExpenseListItem = ({id, description, amount, createdAt}) => (
       <h3>{description}</h3>
     </Link> */}
     <h3><Link to={`/edit/${id}`}>{description}</Link></h3>
-    <p>{amount} - {createdAt}</p>
+    <p>
+      {numeral(amount / 100).format('$0,0.00')}
+      -
+      {moment(createdAt).format('MMMM Do, YYYY')}
+    </p>
   </li>
 );
 
