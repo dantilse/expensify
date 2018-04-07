89bbf467270e0f09094be40fd6be562ae6281ac0
dantilse <dantilse@gmail.com>
2018-04-06 21:38:46 -0500

    Setup test database and hook it all up
    Video 155

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from 'firebase'; // grabs all default exports from Firebase and puts them in a variable
 
 const config = {
-  apiKey: "AIzaSyDsUoZv_dUsTXKcAYG6Bd7eGJpYIcwUIq8",
-  authDomain: "dan-s-expensify.firebaseapp.com",
-  databaseURL: "https://dan-s-expensify.firebaseio.com",
-  projectId: "dan-s-expensify",
-  storageBucket: "dan-s-expensify.appspot.com",
-  messagingSenderId: "328427910673"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
 
 firebase.initializeApp(config);
