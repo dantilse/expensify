1600c8ad288ffbaacb715e52aa6023f1471e2941
dantilse <dantilse@gmail.com>
2018-04-10 21:41:54 -0500

    Add async Edit Expense functionality
    Video 160

--- src/tests/actions/expensese.test.js
+++ src/tests/actions/expensese.test.js
@@ -1,7 +1,16 @@
-import { startAddExpense, addExpense, removeExpense, startRemoveExpense, editExpense, setExpenses, startSetExpenses } from '../../actions/expenses';
-import expenses from '../fixtures/expenses';
 import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
+import {
+  startAddExpense,
+  addExpense,
+  editExpense,
+  startEditExpense,
+  removeExpense,
+  startRemoveExpense,
+  setExpenses,
+  startSetExpenses
+} from '../../actions/expenses';
+import expenses from '../fixtures/expenses';
 import database from '../../firebase/firebase';
 
 const createMockStore = configureMockStore([thunk]);
@@ -14,15 +23,15 @@ beforeEach((done) => {
   database.ref('expenses').set(expensesData).then(() => done());
 });
 
-test('should remove expense', () => {
+test('should setup remove expense action object', () => {
   const action = removeExpense({ id: '123abc' });
   expect(action).toEqual({
     type: 'REMOVE_EXPENSE',
     id: '123abc'
-  })
+  });
 });
 
-test('should remove expense from database', (done) => {
+test('should remove expense from firebase', (done) => {
   const store = createMockStore({});
   const id = expenses[2].id;
   store.dispatch(startRemoveExpense({ id })).then(() => {
@@ -31,14 +40,14 @@ test('should remove expense from database', (done) => {
       type: 'REMOVE_EXPENSE',
       id
     });
-    return database.ref(`expenses/${id}`).once('value')
+    return database.ref(`expenses/${id}`).once('value');
   }).then((snapshot) => {
     expect(snapshot.val()).toBeFalsy();
     done();
   });
 });
 
-test('should edit expense', () => {
+test('should setup edit expense action object', () => {
   const action = editExpense('123abc', { note: 'New note value' });
   expect(action).toEqual({
     type: 'EDIT_EXPENSE',
@@ -49,33 +58,33 @@ test('should edit expense', () => {
   });
 });
 
-// Two test case for add expense, default values and user submitted values
+test('should edit expense from firebase', (done) => {
+  const store = createMockStore({});
+  const id = expenses[0].id;
+  const updates = { amount: 21045 };
+  store.dispatch(startEditExpense(id, updates)).then(() => {
+    const actions = store.getActions();
+    expect(actions[0]).toEqual({
+      type: 'EDIT_EXPENSE',
+      id,
+      updates
+    });
+    return database.ref(`expenses/${id}`).once('value');
+  }).then((snapshot) => {
+    expect(snapshot.val().amount).toBe(updates.amount);
+    done();
+  });
+});
+
 test('should setup add expense action object with provided values', () => {
   const action = addExpense(expenses[2]);
-
   expect(action).toEqual({
     type: 'ADD_EXPENSE',
     expense: expenses[2]
   });
 });
 
-// test('should setup add expense action object with default values', () => {
-//   const action = addExpense();
-//   expect(action).toEqual({
-//     type: 'ADD_EXPENSE',
-//     expense: {
-//       description: '',
-//       note: '',
-//       amount: 0,
-//       createdAt: 0,
-//       id: expect.any(String)
-//     }
-//   });
-// });
-
-// add done for async test cases
 test('should add expense to database and store', (done) => {
-  // create fake Redux store to make correct assertions
   const store = createMockStore({});
   const expenseData = {
     description: 'Mouse',
